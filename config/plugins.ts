74e2b3c1a042f2665a443c56e17f2f1639895182
jake butterworth <jakebutterworth@MacBookAir.lan>
2025-07-16 12:05:41 +1000

    Configure API_TOKEN_SALT and ADMIN_JWT_SECRET in admin config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
-    'users-permissions': {
-      config: {
-        jwtSecret: env('JWT_SECRET'),
-      },
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'), // You might already have this or something similar for admin panel auth
+    },
+    apiToken: {
+      salt: env('API_TOKEN_SALT'), // This is the new crucial line
     },
   });
-  

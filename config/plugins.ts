36f784468b7b4b14acfe442a8f6c23d8378b81e6
jake butterworth <jakebutterworth@MacBookAir.lan>
2025-07-16 13:03:18 +1000

    Integrate Cloudinary upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,23 @@
 export default ({ env }) => ({
-  'users-permissions': {
-    config: {
-      jwtSecret: env('JWT_SECRET'),
+    upload: { // This is the new 'upload' configuration block
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
     },
-  },
-});
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET'),
+      },
+    },
+    // ... other plugins if you have any ...
+  });
